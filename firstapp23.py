b2b098be22c81fc66177e2d259b6500baf869f8e
gmartinolli <102040145+gmartinolli@users.noreply.github.com>
2023-03-13 16:26:04 +0100

    Update firstapp23.py

--- firstapp23.py
+++ firstapp23.py
@@ -1,4 +1,5 @@
 import streamlit as st
 #st.text("hello world")
-st.header("hello world")  
-st.text("from Brixen") 
+#st.header("hello world")  
+#st.text("from Brixen") 
+st.write('The current movie title is', title)
