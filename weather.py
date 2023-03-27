0635af101a38d9b5eb2b52504a679dd0eba76c8d
gmartinolli <102040145+gmartinolli@users.noreply.github.com>
2023-03-27 15:44:56 +0200

    Update weather.py

--- weather.py
+++ weather.py
@@ -7,7 +7,7 @@ APIkey = '07d40238fc934d79750916acad944490'
 location = st.text('Insert a location', )
 
 if location:
-  url = 'http://api.openweather.org.data/2.5/weather?q=' + location + '&appid='+ APIkey + '&units=metric'
+  url = ('http://api.openweather.org.data/2.5/weather?q=' + location + '&appid='+ APIkey + '&units=metric')
 
 
 response = requests.get(url)
