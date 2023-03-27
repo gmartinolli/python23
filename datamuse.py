8cc8d96ba0258add10b7b816768bd9db598917d1
gmartinolli <102040145+gmartinolli@users.noreply.github.com>
2023-03-27 17:05:53 +0200

    Update datamuse.py

--- datamuse.py
+++ datamuse.py
@@ -1,18 +1,18 @@
 import json,requests
 import streamlit as st
 #from pprint import pprint
-keyword=input('Please, insert a keyword ')
+keyword= st.input('Please, insert a keyword ')
 option = st.selectbox("Choose one option",
                        ('similar meaning', 'sounds like', 'rhymes with')
-if option == 'similar meaning':
-                      key = 'rel_syn='
-elif option == 'sounds like':
-                      key = 'sl='
-elif option == 'rhymes with':
-                      key = 'rel_rhy='
-else:
-                      pass
-                      
+if option:
+             if option =='similar meaning':
+                 key='rel_syn='
+             elif option == 'sounds like':
+                  key='sl='
+             elif option == 'rhymes with':
+                      key='rel_rhy='
+             else:
+                   key = None                      
 url= 'https://api.datamuse.com/words?'+key + keyword
 response = requests.get(url)  
 dataFromDatamuse = json.loads(response.text)
