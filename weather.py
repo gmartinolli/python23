338e0faf10d5efed692b4fcc41a06c5f809a6a48
gmartinolli <102040145+gmartinolli@users.noreply.github.com>
2023-03-27 16:19:10 +0200

    Update weather.py

--- weather.py
+++ weather.py
@@ -7,8 +7,8 @@ APIkey = '07d40238fc934d79750916acad944490'
 location = st.text('Insert a location', )
 
 if location:
-  url = ('http://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid='+ APIkey + '&units=metric')
-
+  #url = ('http://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid='+ APIkey + '&units=metric')
+  url = 'http://api.openweathermap.org/data/2.5/weather?q=Bolzano&appid=07d40238fc934d79750916acad944490&units=metric'
 
 response = requests.get(url)
 
