976d95ee289a1bd0b6a91c9565b8868c9c1cf698
gmartinolli <102040145+gmartinolli@users.noreply.github.com>
2023-04-17 15:48:33 +0200

    Update gtts.py

--- gtts.py
+++ gtts.py
@@ -1,10 +1,10 @@
-#from gtts import gTTS
+from gtts import gTTS
 from googletrans import Translator
 import streamlit as st
 #import IPython.display as ipd
 
 translator = Translator()
-tts1=gTTS (text='hello world')
+tts1=gTTS(text='hello world')
 tts1.save('english_hello.mp3')
 #ipd.display(ipd.Audio('english_hello.mp3'))
 st.write('english text')
