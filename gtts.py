a732b5417431426831cc9416587c200822bf9177
gmartinolli <102040145+gmartinolli@users.noreply.github.com>
2023-04-17 15:20:18 +0200

    Update gtts.py

--- gtts.py
+++ gtts.py
@@ -3,5 +3,6 @@ from googletrans import Translator
 import streamlit as st
 
 translator = Translator()
-tts1=gTTS (text='hello world')tts1.save('english_hello.mp3')
+tts1=gTTS (text='hello world')
+tts1.save('english_hello.mp3')
 st.write('audio example')
