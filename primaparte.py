a38c37abfd332bdb3281114aac796b1a9e90a96f
gmartinolli <102040145+gmartinolli@users.noreply.github.com>
2023-05-08 15:38:52 +0200

    Create primaparte.py

---
+++ primaparte.py
@@ -0,0 +1,18 @@
+import st as streamlit
+from gtts import gTTS
+from googletrans import Translator
+
+st.header("Lavoriamo sulla pronuncia inglese")
+st.write("App per lavorare sulla tua pronuncia inglese.")    
+welcome = st.radio("Scegli un\'azione",('scrivi una parola','registra parola', 'traduci parola')
+                   if welcome == 'scrivi una parola'
+                   st.write = ('scrivi')
+                   if welcome == 'registra parola'
+                   st.write = ('registra')
+                   if welcome == 'traduci parola'
+                   st.write = ('traduci')
+                   else:
+                   st.write = ('scegliere un\'azione')
+                   
+           
+                
