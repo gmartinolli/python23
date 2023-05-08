ea4454e4ae8ff29daefe011066db5e41b52047b0
gmartinolli <102040145+gmartinolli@users.noreply.github.com>
2023-05-08 15:56:08 +0200

    Update primaparte.py

--- primaparte.py
+++ primaparte.py
@@ -9,11 +9,11 @@ welcome = st.radio(
   "Scegli un\'azione",
   ('scrivi una parola','registra parola', 'traduci parola'))
 if welcome == 'scrivi una parola':
-    st.text_input()
+    st.text_input("scrivi")
 if welcome == 'registra parola':
     st.write("registra")
 if welcome == 'traduci parola':
-    st.text_input()
+    st.text_input("scrivi")
 else:
     st.write("scegliere un\'azione")
                    
