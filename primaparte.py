956ec80c31142299e24cdabb99b60f9d3b4f57a4
gmartinolli <102040145+gmartinolli@users.noreply.github.com>
2023-05-08 15:48:31 +0200

    Update primaparte.py

--- primaparte.py
+++ primaparte.py
@@ -7,14 +7,14 @@ st.write("App per lavorare sulla tua pronuncia inglese.")
 welcome = st.radio(
   "Scegli un\'azione",
   ('scrivi una parola','registra parola', 'traduci parola'))
-                   if welcome == 'scrivi una parola':
-                      st.write("scrivi")
-                   if welcome == 'registra parola':
-                       st.write("registra")
-                   if welcome == 'traduci parola':
-                       st.write("traduci")
-                   else:
-                       st.write("scegliere un\'azione")
+  if welcome == 'scrivi una parola':
+    st.write("scrivi")
+  if welcome == 'registra parola':
+    st.write("registra")
+  if welcome == 'traduci parola':
+    st.write("traduci")
+  else:
+    st.write("scegliere un\'azione")
                    
            
                 
