a97ea2b312d7ef60fe7161efc868b80bafcbe246
gmartinolli <102040145+gmartinolli@users.noreply.github.com>
2023-05-17 10:17:52 +0200

    Update recorder.py

--- recorder.py
+++ recorder.py
@@ -5,7 +5,7 @@ from audio_recorder_streamlit import audio_recorder
 registratore = audio_recorder()
 audio_recorder = st.audio(registratore, format="audio/wav")
 if registratore:
-  avvia_registrazione
+  avvia_registrazione = st.write("avvia")
   avvia_registrazione.save("my_audio.mp3")
   
  
