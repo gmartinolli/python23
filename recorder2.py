f025d0c8dc9675f35b519108d712964c03bfa05a
gmartinolli <102040145+gmartinolli@users.noreply.github.com>
2023-05-17 11:47:30 +0200

    Update recorder2.py

--- recorder2.py
+++ recorder2.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from audiorecorder import audiorecorder
 import base64
+import http.client
 
 st.title("Audio Recorder")
 audio = audiorecorder("Click to record", "Recording...")
@@ -16,3 +17,13 @@ if len(audio) > 0:
   
     enc = base64.b64encode(open("audio.mp3", "rb").read())
     st.write(enc)
+
+
+conn = http.client.HTTPSConnection("pronunciation-assessment1.p.rapidapi.com")
+
+payload =  enc
+
+conn.request("POST", "/pronunciation", payload, headers)
+
+res = conn.getresponse()
+data = res.read()
