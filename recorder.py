9b56bc01ed9fe36a0df650e2d52cb9b7211fbb79
gmartinolli <102040145+gmartinolli@users.noreply.github.com>
2023-05-17 11:11:36 +0200

    Update recorder.py

--- recorder.py
+++ recorder.py
@@ -1,6 +1,8 @@
 import json,requests
 import streamlit as st
 from audio_recorder_streamlit import audio_recorder
+#APIkey = x-rapidapi-key
+#url = 'https://rapidapi.com/developer/security/default-application_7420963' + + '&appid=' + APIkey
 
 registratore = audio_recorder()
 audio_recorder = st.audio(registratore, format="audio/wav")
