c425479e0262d5cce8b1194262fe0e4ea0629708
gmartinolli <102040145+gmartinolli@users.noreply.github.com>
2023-05-17 11:41:05 +0200

    Update recorder2.py

--- recorder2.py
+++ recorder2.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from audiorecorder import audiorecorder
+import base64
 
 st.title("Audio Recorder")
 audio = audiorecorder("Click to record", "Recording...")
@@ -11,3 +12,7 @@ if len(audio) > 0:
     # To save audio to a file:
     wav_file = open("audio.mp3", "wb")
     wav_file.write(audio.tobytes())
+
+  
+    enc = base64.b64encode(open("audio.mp3", "rb").read())
+    st.write(enc)
