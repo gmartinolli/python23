a7ca05d9b77f827247927a84e528771c671a3434
gmartinolli <102040145+gmartinolli@users.noreply.github.com>
2023-05-17 11:25:31 +0200

    Update recorder.py

--- recorder.py
+++ recorder.py
@@ -5,7 +5,7 @@ from audio_recorder_streamlit import audio_recorder
 #url = 'https://rapidapi.com/developer/security/default-application_7420963' + '&appid=' + APIkey
 
 registratore = audio_recorder()
-audio_recorder = st.audio(registratore, format="audio/wav")
+audio_recorder = st.audio(registratore, format="audio/mp3")
 if registratore:
   avvia_registrazione = st.write("avvia")
   avvia_registrazione.save("my_audio.mp3")
