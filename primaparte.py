092ccd4604ef1543f7ae85471b5bec36e07d641d
gmartinolli <102040145+gmartinolli@users.noreply.github.com>
2023-05-17 16:06:20 +0200

    Update primaparte.py

--- primaparte.py
+++ primaparte.py
@@ -5,7 +5,7 @@ from googletrans import Translator
 
 
 st.header("Lavoriamo sulla pronuncia inglese")
-st.write("App per lavorare sulla tua pronuncia inglese. Per iniziare scegli quale azione vuoi fare:") 
+st.write("app per lavorare sulla tua pronuncia inglese. Per iniziare scegli quale azione vuoi fare:") 
 
 welcome = st.radio(
   "Scegli un\'azione",
