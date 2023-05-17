af6be1c75ffa88ea14a7d9fe2ce795850d4495bf
gmartinolli <102040145+gmartinolli@users.noreply.github.com>
2023-05-17 15:48:01 +0200

    Update primaparte.py

--- primaparte.py
+++ primaparte.py
@@ -14,17 +14,35 @@ if welcome == 'scrivi una parola':
     
 if welcome == 'registra parola':
     st.write("registra")
+
+translator = Translator()
+
+word = st.text_input("Please, insert a word/sentence")
+
+
+
+if (word and lang_code):
+  translate = translator.translate(word, dest=lang_code)
+  tts1=gTTS(text=translate.text, lang=lang_code)
+  tts1.save('file.mp3')
+
+  
+    
     
 if welcome == 'traduci parola':
-    word = st.text_input("scrivi")
     translator = Translator()
-    #word = st.text_input('Type some text:')
-    language = st.text_input('scegli lingua')
-    if language:
-      translation = translator.translate(word, dest=language)
+   
+    word = st.text_input("cosa vuoi tradurre?")
     
-      tts1=gTTS(word=translation.text, dest=language)
-      st.write(tts1)
+    #word = st.text_input('Type some text:')
+    lang_code = st.text_input("scegli una lingua: en = inglese, de = tedesco")
+    if (word and lang_code):
+      translation = translator.translate(word, dest=lang_code)
+      tts1=gTTS(text=translation.text, lang=language_code)
+      tts1.save('file.mp3')
+      audio_file = open("file.mp3", "rb")
+      st.audio(data=audio_file, format="audio/mp3", start_time=0)    
+      st.write = tts1
 #else:
    # st.write("scegliere un\'azione")
                    
