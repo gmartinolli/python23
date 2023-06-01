80d49157d1363952cca69349c816f71cd098da9f
gmartinolli <102040145+gmartinolli@users.noreply.github.com>
2023-06-01 17:47:03 +0200

    Update primaparte.py

--- primaparte.py
+++ primaparte.py
@@ -1,5 +1,8 @@
 import json,requests
 import streamlit as st
+import dictionaryo
+import python-vlc
+from dictionaryio import DictionaryIO
 from gtts import gTTS
 from googletrans import Translator
 
@@ -10,7 +13,7 @@ st.subheader("app per lavorare sulla tua pronuncia inglese. Per iniziare scegli 
 
 welcome = st.radio(
   "Scegli un\'azione",
-  ('scrivi una parola','registra parola', 'traduci parola'))
+  ('scrivi una parola','registra parola', 'traduci parola', 'come si pronuncia?'))
 if welcome == 'scrivi una parola':
     st.text_input("scrivi")
     
@@ -29,6 +32,16 @@ if welcome == 'traduci parola':
       audio_file = open("file.mp3", "rb")
       st.audio(data=audio_file, format="audio/mp3", start_time=0)    
       st.write = tts1
+   
+  if welcome == 'come si pronuncia?':
+    word = st.text_input("scrivi")
+    if (word):
+      word.DictionaryIO()
+      word.phonetics()
+      st.write = word.phonetics()
+#word.meaning()
+    
+    
 #else:
    # st.write("scegliere un\'azione")
                    
