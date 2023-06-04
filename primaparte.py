91ab918697951a659fc19443e66624b2472d2e4d
gmartinolli <102040145+gmartinolli@users.noreply.github.com>
2023-06-04 23:04:56 +0200

    Update primaparte.py

--- primaparte.py
+++ primaparte.py
@@ -19,7 +19,7 @@ if welcome == 'scrivi una parola':
 if welcome == 'come si pronuncia?':
     word = st.text_input("scrivi")
     if (word):
-       word.DictionaryIO()
+       word.DictionaryIO() = word
        word.phonetics()
        st.write = word.phonetics()    
           
